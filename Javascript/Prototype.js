a11c025849d7164cbf843f33dcee9db91233ae39
Mahesh Kumar <ml4909854@gmail.com>
2026-02-08 12:59:10 +0530

    code

--- Javascript/Prototype.js
+++ Javascript/Prototype.js
@@ -1,16 +1,16 @@
-function Person(first, last, age, eyecolor) {
-  this.firstname = first;
-  this.lastname = last;
-  this.age = age;
-  this.eyecolor = eyecolor;
-  this.country = "India";
-  this.fullname = function(){
-    return this.firstname + " " + this.lastname
-  }
-}
+// function Person(first, last, age, eyecolor) {
+//   this.firstname = first;
+//   this.lastname = last;
+//   this.age = age;
+//   this.eyecolor = eyecolor;
+//   this.country = "India";
+//   this.fullname = function(){
+//     return this.firstname + " " + this.lastname
+//   }
+// }
 
-let myfather = new Person("Pratap", "Lodhi", 55, "brown");
-myfather.nationality = "Indian";
+// let myfather = new Person("Pratap", "Lodhi", 55, "brown");
+// myfather.nationality = "Indian";
 // let full = myfather.fullname()
 // console.log(full)
 
@@ -19,12 +19,29 @@ myfather.nationality = "Indian";
 //     return this.firstname + " " + this.lastname
 // }
 
-// add properties using prototype
-Person.prototype.city = "Bhopal"
+// // add properties using prototype
+// Person.prototype.city = "Bhopal"
 
 
-Person.prototype.method = function(){
-    return this.age
+// Person.prototype.method = function(){
+//     return this.age
     
+// }
+// console.log(myfather);
+
+
+// classs contgructore
+
+class CarName{
+  constructor(name , year){
+     this.name = name
+     this.year = year
+  }
+  age()  {
+       const date = new Date()
+        return date.getFullYear() - this.year;
+  }
 }
-console.log(myfather);
+
+const Mahesh = new CarName("Volvo" , 2000)
+console.log(Mahesh.age())
