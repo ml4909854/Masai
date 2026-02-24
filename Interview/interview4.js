2c7a37b32e2be4237502dc6c7b4e27aa978724b0
Mahesh Kumar <ml4909854@gmail.com>
2026-02-24 16:35:29 +0530

    code

---
+++ Interview/interview4.js
@@ -0,0 +1,19 @@
+
+
+// setTimeout(() => {
+//     console.log("mahesh")
+// });
+
+// let res = isNaN("Hi")
+// console.log(res)
+// let res1 = isNaN(2)
+// console.log(res1)
+
+
+// x = 10
+// console.log(x)
+// let x = 10; // it is going into a temperal dead zone
+
+console.log(x)
+let x;
+console.log(x)
