1dbb618e6f61230b8c938630e239274583d89733
Mahesh Kumar <ml4909854@gmail.com>
2026-04-09 13:20:17 +0530

    code

---
+++ Interview/interview7.js
@@ -0,0 +1,159 @@
+// console.log("mahesh")
+
+
+// let a = []
+// let b = 
+// console.log(a===b)
+
+// let a;
+// let b;
+// console.log(a == b)
+
+
+// let data = {name:"javascript"}
+// console.log(delete data.name)
+// console.log(data)
+
+
+// let data = {name: "javascript"}
+// console.log(delete data)
+
+
+// let arr = [1 ,2 ,3 ,4 ,5]
+// console.log(delete arr) itw ill also not works
+// becuase delete not work in the case of varble declare with let , const ,var
+// console.log(delete arr[0]) it will delete first item in the array
+// console.log(arr)
+
+// destruture the arr
+// this is the following way to destructure the arrays.
+// let [n1 , n2 , ...n3] = arr
+// // console.log(n1)
+// // console.log(n2 )
+// // console.log(n3)
+
+// 
+// let a = null || {}||true
+//  console.log(a)
+// // 👉 In JavaScript, || (OR operator):
+
+// // Returns the first truthy value
+// // It does NOT return only true/false
+
+// // console.log([] == false)
+// // console.log([] === false)
+
+
+// // console.log(!!"hello")
+
+// // console.log([] == ![])
+// // console.log({name:"mahesh"} + {})
+// // console.log([] + [])
+
+
+
+// // ===============================
+// // 🔥 JavaScript Interview Questions
+// // ===============================
+
+// // Q1
+// console.log("Q1:", [] == false)
+
+// // Q2
+// console.log("Q2:", [] === false)
+
+// // Q3
+// let q3 = {} + []
+// console.log("Q3:", q3)
+
+// // Q4
+// let q4 = [] + {}
+// console.log("Q4:", q4)
+
+// // Q5
+// console.log("Q5:", typeof null)
+
+// // Q6
+// console.log("Q6:", typeof NaN)
+
+// // Q7
+// try {
+//   let a = 10
+//   (function () {
+//     console.log("Q7:", a)
+//     let a = 20
+//   })()
+// } catch (e) {
+//   console.log("Q7: Error ->", e.message)
+// }
+
+// // Q8
+// console.log("Q8:", 1 + "2" + "2")
+
+// // Q9
+// console.log("Q9:", 1 + +"2" + "2")
+
+// // Q10
+// console.log("Q10:", "A" - "B" + "2")
+
+// // Q11
+// console.log("Q11:", true + false)
+
+// // Q12
+// console.log("Q12:", !!"hello")
+
+// // Q13
+// let obj = { a: 1 }
+// let b = obj
+// b.a = 5
+// console.log("Q13:", obj.a)
+
+// // Q14
+// console.log("Q14:", [] == ![])
+
+// // Q15
+// let q15 = "5" - 3
+// console.log("Q15:", q15)
+
+// // Q16
+// console.log("Q16:", null == undefined)
+
+// // Q17
+// console.log("Q17:", null === undefined)
+
+// // Q18
+// console.log("Q18:", typeof [])
+
+// // // Q19
+// // console.log("Q19:", [] + [])
+
+// // // Q20
+// // console.log("Q20:", {} == {})
+
+
+// let number = 123
+// let str = number.toString()
+// // let str = String(number)
+// // let str = number + ""
+
+// let newStr = str.split("")
+// console.log(newStr)
+// let bag = ""
+// for(let i = newStr.length-1 ; i>=0 ; i--){
+//     bag  = bag+str[i]
+// }
+// let updatedNum = +bag 
+// console.log(typeof updatedNum)
+
+// console.log(x)
+// console.log(y)
+
+// var x = y = 10
+
+// x = 10 
+// y = 13
+// console.log(x , y)
+
+// console.log("3" + 12 * 8)
+
+
