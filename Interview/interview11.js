72ed6eef82ffce22aae7611034a96ab235284956
Mahesh Kumar <ml4909854@gmail.com>
2026-04-11 11:33:23 +0530

    code

---
+++ Interview/interview11.js
@@ -0,0 +1,98 @@
+
+
+// let arr = [1 , 2 ,3, 4][1  ,3 ,1 ]
+// console.log(arr) this will take last part as a index and 
+// thats why it is return 2 as a output
+
+
+// let obj = {
+//     1:"a" , 
+//     2:"b",
+//     "b":"b",
+//     [1]:"b"
+// }
+
+// console.log(obj)
+
+
+// {
+//     function fn(){
+//         console.log("hello")
+//     }
+// } output is heeloo we can call a funtion anywhere elese where we want 
+
+// fn()
+
+
+// 'use strict'
+// let a = 19 
+// let b = 39
+// c = 13 It give me an error becuase use strict check javascritp 
+// very strictly we are not defined any varibale that why resyt 
+// us c is undefined
+// console.log(a , b , c)
+
+
+// function fn(){
+//     return 100
+// }
+// var fn;
+// console.log(fn) because we are not called a funtion here 
+// we have to call a funtion 
+
+// function fn(){
+//     return 100
+// }
+
+// var fn;
+// console.log(fn())
+
+// write a ternary operatore
+
+// let age = 500
+
+// let res = age>10?"Eligible":"Not eligible"
+
+// console.log(res)
+
+
+// reduce async function  (params) {
+    
+// }
+
+// let arr = [1  , 2, 3, 4, 5 , 10]
+// let res = arr.reduce((acc , cur)=>{
+//         return acc+=cur
+// }) This is the overall output of the code let move on the other direction of the code
+
+
+
+// console.log(res)
+
+
+// let str1 = "hello2026"
+// let str2 = "world"
+
+// let min = Math.min(str1.length  , str2.length)
+// let bag = ""
+// for(let i = 0 ; i<min; i++){
+//     bag+= str1[i] + str2[i]
+// }
+// let res = str1.slice(str2.length)
+// // bag += res
+// // console.log(bag)
+
+
+// let str1 = "hello2029"
+// let str2 = "worldsdfsdjfff"
+// let bag = ""
+// let maxlength = Math.max(str1.length , str2.length)
+// for(let i = 0 ; i<maxlength; i++){
+//     if(str1[i]){
+//         bag+=str1[i]
+//     }
+//     if(str2[i]){
+//         bag+=str2[i]
+//     }
+// }
+// console.log(bag)
---
+++ Interview/interview12.js
@@ -0,0 +1,34 @@
+
+
+// console.log("mahesh")
+
+// {
+//     function fn(a , b){
+//         "use strict"
+//         let a = 100; b=300
+//         return arguments[0] + arguments[1]
+//         console.log(a+b)
+//     }
+// }
+
+// console.log(fn(10 , 20))
+
+
+function sum(a = 1000 , b=100){
+    console.log(a+b)
+}
+
+// console.log(sum(undefined  , 3))   //It will give me a undefined because funtion doesnot returned anything
+/// it will give me a output as wll but reutrn ed a undefined with the output
+// because objects is not returning anything an we consoleit that why 
+// they give me undefined 
+
+// the value of argument not changeg it will reamin same it will reamin undefined as well
+
+// console.log(null + 3) it reutnr 3
+// console.log(null + "abc") it takes null as a string
+
+// console.log(undefined + 1) there is comes into a not a number cases n
+// now move on the some other topic as well wehere i discuss wome pupular words as well lets move there
+
+// console.log(undefined + "sab")
