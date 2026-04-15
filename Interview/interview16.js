10d3dde80eaa7402375e5b1b3427319a28a8ec3b
Mahesh Kumar <ml4909854@gmail.com>
2026-04-15 17:32:13 +0530

    code

--- Interview/interview16.js
+++ Interview/interview16.js
@@ -3,3 +3,14 @@
 // let arr  = [2 ,1 , 4, 3]
 // let res = arr.filter((ele)=>ele ==2)
 // console.log(res) filter use filter gives us to filter array
+
+    // let arr  = [1 , 3, 4, 5]
+    // let res = arr.findIndex((ele)=> ele == 4)
+    // console.log(res) // it is used to find the element
+
+
+    // 
+
+    let arr = [1 , 2,3 ,4,5 ]
+    let res = arr.filter((ele)=>ele=6)
+    console.log(res)
