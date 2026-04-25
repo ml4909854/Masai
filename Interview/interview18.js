15c367a5d8ca36e2f7a57f79eb62deddaa82c8bb
Mahesh Kumar <ml4909854@gmail.com>
2026-04-25 11:59:12 +0530

    code

---
+++ Interview/interview18.js
@@ -0,0 +1,30 @@
+// console.log("Mahesh")
+
+// let arr = [1 ,2, 3, 4, 5]
+// console.log(arr.includes(3 , -3))
+// console.log(arr.includes(20))
+
+// ? otpitonal chaining
+
+// let num1  = 3
+// let num2 = 10
+// num1 = temp
+// temp = num2
+// num2 = num1
+// console.log(num1)
+
+// let a = 1;
+// let b = 2;
+
+// // []
+// [a , b] = [b , a]
+// console.log(a , b)
+
+// const arr = ["rohit" , "hites"]
+// for(let i = 0 ; i<arr.length ; i++){
+//     if(arr[0].includes(arr[1])){
+//         console.log(true)
+//     }else{
+//         console.log("false")
+//     }
+// }
