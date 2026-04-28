0cfc94ce051f64e42f1b8d575fffe287362f3e98
Mahesh Kumar <ml4909854@gmail.com>
2026-04-28 10:46:38 +0530

    code

---
+++ Interview/interview19.js
@@ -0,0 +1,107 @@
+// Shallow Copy
+// Copies only first level
+// Nested objects shared
+// ✅ Deep Copy
+// Full copy (no shared reference)
+// ✅ Generator Function
+// Function that can pause/resume using yield
+
+
+// let str = "eeeeeeaaaaaaaadf"
+// let freq = {}
+
+// for(let char of str){
+//      if(char === " ")continue
+//     if(freq[char]){
+//         freq[char] +=1
+//     }else{
+//         freq[char] = 1
+//     }
+// }
+// console.log(freq)
+
+// lets go and count the fe
+
+// let arr = [1,,2,2,4,4,2,2,3,5,2,4,5,2,1,1,35,5,2,35,34,3,2,3,3,4,]
+// let freq = {}
+// for(let char of arr){
+//     if(char === undefined)continue
+//     if(freq[char]){
+//         freq[char] +=1
+//     }else{
+//         freq[char] = 1
+//     }
+// }
+// console.log(freq)
+
+
+// let arr = [ [0 , 1] , ["sangha" , 1]  , ["rakha"  , "king"]]
+// console.log(arr)
+
+// let newArr = []
+// for(let i= 0 ; i<arr.length; i++){
+//     for(let j = 0 ; j<arr[i].length; j++){
+//         newArr.push(arr[i][j])
+//     }
+// // }
+// // console.log(newArr)
+
+// if([] ){
+//     console.log("mahesh")
+// }else{
+//     console.log("Nothing printed")
+// }
+// if([] ==true){
+//     console.log("mahesh")
+// }else{
+//     console.log("Nothing printed")
+// }
+// if([] == false ){
+//     console.log("mahesh")
+// }else{
+//     console.log("Nothing printed")
+// }
+
+
+
+// console.log(typeof null)
+
+
+// console.log(typeof undefined)
+
+// console.log(null == undefined)
+// console.log(null === undefined)
+
+// let arr = [1  , 2 , 3,100, 200 , 300]
+// // console.log(arr.sort())
+// let res = arr.sort((a , b)=>a - b)
+// console.log(res) this is the answer of the question 
+
+// let str = "mahesh"
+// console.log(str.indexOf("h")) // it return it first occurence index
+// console.log(str.indexOf("j")) // it give -1
+// // console.log(str.charAt(3)) // here we enter a some index
+// console.log(str.charAt(9)) it does not return anything 
+
+
+// console.log({}=={})
+// console.log([] == [])
+// console.log([]===[])
+
+// let str = "This is an interview"
+// let arr = str.split(" ")
+// let word = ""
+// for(let i = 0 ; i<arr.length ; i++){
+//     // console.log(arr[i].length) 
+//     if(arr[i].length > word.length ){
+//       word = arr[i]
+//     }
+   
+// }
+// // console.log(word)
+
+// let arr = [1 ,2 ,3 ,4 ,5]
+// let double = arr.map((item)=>item = 5)
+// console.log(double)
+
+
