4c0d62425599a7e5d7c8f9798aab01cbbe11ed77
Mahesh Kumar <ml4909854@gmail.com>
2026-04-30 12:19:18 +0530

    code

---
+++ Interview/interview21.js
@@ -0,0 +1,52 @@
+// console.log("Inteview21.js")
+
+// let arr = [1 , 12 , 3 ,53 ,5 ,6]
+// // sort array without using an inbuilt method
+// // sort the array  using a bubble sort method
+// for(let i = 0 ; i<arr.length; i++){
+//     for(let j = 0; j<arr.length-1; j++){
+//        if(arr[j] > arr[j+1]){
+//          let temp = arr[j]
+//          arr[j] = arr[j+1]
+//          arr[j+1] = temp
+//        }
+//     }
+// }
+// console.log(arr)
+
+// let str1 = "mahesh"
+// let str2 = "1000"
+
+// let max = Math.max(str1.length , str2.length)
+// let bag = ""
+// for(let i = 0 ; i<max ; i++){
+//       if(str1[i]){
+//         bag+= str1[i]
+//       }
+//       if (str2[i]){
+//         bag+= str2[i]
+//       }
+// }
+// console.log(bag)
+
+// let arr = [15 , 20 , 35 , 60 , 30]
+
+// let k = 125
+// for(let i = 0 ; i<arr.length; i++){
+//     let sum  = 0
+//      for(let j = i ; j<arr.length; j++){
+           
+//             sum += arr[j]
+//             if(sum === k ){
+//                let start = i 
+//                let end = j 
+//                let res = ""
+//                for(let k = start ; k<=end; k++){
+//                  console.log(arr[k])
+//                }
+//                console.log(res)
+//             }
+//      }
+    
+
+// }
