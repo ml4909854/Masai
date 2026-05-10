466062d4cf955b7153823e58d39e0ef0afd5f327
Mahesh Kumar <ml4909854@gmail.com>
2026-05-10 14:30:31 +0530

    code

---
+++ Interview/interview24.js
@@ -0,0 +1,35 @@
+
+// console.log("interview24.js")
+
+// // closure examples
+
+// function outer(){
+//     let message = "hello"
+//     function inner(){
+//         console.log(message)
+//     }
+//     return inner
+// }
+
+// const myFunction = outer() // outer function calls here
+// myFunction()
+
+// timer examples of closure
+// let count = 0 
+// function counter(){
+//     console.log(count+=1)
+// }  closure create fron outerscope
+// counter()
+// // counter()
+// // counter()
+
+// function outer(){
+//     let counter = 0 
+//     function inner(){
+//         console.log(counter+=1)
+//     }
+//     return inner
+// }
+//    let myFunction = outer() // outer function called here
+//    myFunction()
+//    myFunction()
