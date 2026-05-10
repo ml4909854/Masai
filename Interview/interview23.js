00b67bf2247f8160bb1e8556ac857eb4a9d31253
Mahesh Kumar <ml4909854@gmail.com>
2026-05-10 13:00:55 +0530

    code

---
+++ Interview/interview23.js
@@ -0,0 +1,77 @@
+
+// // funtion scoped not local scoped
+// var num = 100
+// {
+// var num  = 200 // var is not block scroped
+// }
+// console.log(num)
+
+// var num = 100
+// function scope(){
+//     var num = 200
+//     console.log(num)
+// }  // var is a function scoped
+// console.log(num)
+// scope()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(undefined + 1)
+// console.log(null  == {})  
+
+
+// let arr = function(){
+//     console.log("print function")
+// }
+
+// if(arr()){
+//     console.log("printed")
+// }else{
+//     console.log("Not printed")
+// }
+// console.log(null == undefined)
+
+
+// can be declare
+
+// var num = 100
+// var num = 300
+// console.log(num)
+
+// can be reassingend 
+
+// var num = 100
+//   num = 3499   var can be reassined
+//   console.log(num)
+
+// func()  function with var keyword are hoisete
+// var func = function(){
+//     console.log(
+//     "print function "
+//     )
+// }
+// func()
+// console.log(num)
+// var num = 100
+
+// console.log(printFunction)
+// var  printFunction = function(){
+//     return "print fucntion"
+// // }
+// printFunction()
+// function printFunction(){
+//     console.log("hello world!")
+// }
+// printFunction()
+// but here variable takes it undefined there is not printed 
+// printefuntion is a valrivle and you call it thats why print function gives us undeifn eouted
